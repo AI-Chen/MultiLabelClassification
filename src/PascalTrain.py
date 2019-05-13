81cbd9f9c229918606c6b88d81f4863f70150309
pys16 <pys19980228@gmail.com>
2019-05-13 16:13:06 +0800

    Enable model choices in argument

--- src/PascalTrain.py
+++ src/PascalTrain.py
@@ -17,20 +17,22 @@ from sklearn.metrics import average_precision_score
 import torch
 from torch.autograd import Variable
 import torchvision.transforms as transforms
-import torchvision.models as models
+# import torchvision.models as models
 
 # import multiprocessing
 CORES = 4  # int(float(multiprocessing.cpu_count())*0.25)
 
 # os.chdir('/export/home/bbrattol/git/JigsawPuzzlePytorch/Pascal_finetuning')
 # from PascalLoader import DataLoader
-from Network import resnet18
+from Network import resnet18, resnet34, resnet50, resnet101, resnet152
 from Utils import MyDataLoader, adjust_learning_rate, load_model_from_file
 
 parser = argparse.ArgumentParser(description='Train network on Pascal VOC 2012')
 parser.add_argument('pascal_path', type=str, help='Path to Pascal VOC 2012 folder')
 parser.add_argument('--finetune', default=None, type=int, help='whether to use pytorch pretrained model and finetune')
-parser.add_argument('--model', default=None, type=str, help='pretrained model path')
+parser.add_argument('--model', default='resnet18', type=str, help='which backbone network to use',
+                    choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'])
+parser.add_argument('--modelpath', default=None, type=str, help='pretrained model path')
 # parser.add_argument('--freeze', dest='evaluate', action='store_true', help='freeze layers up to conv5')
 # parser.add_argument('--freeze', default=None, type=int, help='freeze layers up to conv5')
 parser.add_argument('--fc', default=None, type=int, help='load fc6 and fc7 from model')
@@ -47,7 +49,13 @@ args = parser.parse_args()
 #    '--gpu','0',
 # ])
 prefix = time.strftime("%y%m%d_%H%M", time.localtime())
-
+models ={
+    'resnet18': resnet18,
+    'resnet34': resnet34,
+    'resnet50': resnet50,
+    'resnet101': resnet101,
+    'resnet152': resnet152
+}
 
 def compute_mAP(labels, outputs):
     y_true = labels.cpu().numpy()
@@ -103,13 +111,10 @@ def main():
     iter_per_epoch = int(N/args.batch)
 
     # Network initialize
-    # net = resnet18(pretrained=True, num_classes=20)
-    # net = models.resnet18(pretrained=True if args.finetune is not None else False)
-
     # finetune: freeze some layers and modify the fc layer.
     if args.finetune is not None:
         # Initialize the network
-        net = models.resnet18(pretrained=True)
+        net = models[args.model](pretrained=True)
         # Freeze layers up to conv4
         for i, (name, param) in enumerate(net.named_parameters()):
             if 'conv' in name or 'features' in name:
@@ -118,11 +123,11 @@ def main():
         in_channel = net.fc.in_features
         net.fc = torch.nn.Linear(in_features=in_channel, out_features=20)
 
-    elif args.model is not None:
-        net = load_model_from_file(args.model, args.fc)
+    elif args.modelpath is not None:
+        net = load_model_from_file(args.modelpath, args.fc)
 
     else:
-        net = models.resnet18(pretrained=False, num_classes=20)
+        net = models[args.model](pretrained=False, num_classes=20)
 
     if args.gpu is not None:
         net.cuda()
@@ -174,7 +179,7 @@ def main():
             steps += 1
         
         if epoch % 5 == 0:
-            filename = '%s/%s_%03i.pth' % (args.checkpoint, prefix, epoch+1)
+            filename = '%s/%s_%s_%03i.pth' % (args.checkpoint, args.model, prefix, epoch+1)
             torch.save(net.state_dict(), filename)
             print('Saved: '+args.checkpoint)
         
