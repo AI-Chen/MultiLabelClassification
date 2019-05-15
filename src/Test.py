53234af54bed45fa16255c500202f2d7d0f4a209
wcy666687 <624715810@qq.com>
2019-05-15 19:00:09 +0800

    new test

--- src/Test.py
+++ src/Test.py
@@ -6,10 +6,45 @@ from PIL import Image
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 
-from Utils import load_model_from_file
+from Utils import load_model_from_file, MyDataLoader
+
+def gethreshold( val_loader,transform, gpu=None,  model_path='../checkpoints/190512_1711_011.pth',  model="resnet18"):
+    net = load_model_from_file(model_path, model, True)
+    if gpu is not None:
+        net.cuda()
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+
+    net.eval()
+    ymin=np.zeroes(20)
+    ymax=np.linspace(-100,-100,20)
+    for i, (images, labels) in enumerate(val_loader):
+        images = transform(images)
+        images = images.view((-1, 3, 224, 224))
+        if gpu is not None:
+            images = images.cuda()
+
+        outputs = net(images)
+        outputs = outputs.cpu().data
+        outputs = outputs.view((-1, 20))
+        y_true = labels.cpu().numpy()
+        y_pred = outputs.cpu().numpy()
+        for j in range(20):
+            if y_true[j]==0:
+                if y_pred[j]>ymax[j]:
+                    ymax[j]=y_pred[j]
+            else:
+                if y_pred[j]<ymin[j]:
+                    ymin[j]=y_pred[j]
+
+        if i%20==0:
+            print('ymin:',ymin)
+            print('ymax:',ymax)
+    return ymin,ymax
 
 
-def test(transform, model_path='../checkpoints/190508_0909_001.pth', img_path='../test.jpg', model="resnet18", gpu=None):
+
+def test(transform, model_path='../checkpoints/190512_1711_011.pth', img_path='../test.jpg', model="resnet18", gpu=None):
     net = load_model_from_file(model_path, model, True)
     if gpu is not None:
         net.cuda()
@@ -36,11 +71,18 @@ if __name__ == '__main__':
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
     val_transform = transforms.Compose([
-            #transforms.Scale(256),
-            #transforms.CenterCrop(227),
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ])
-    test(val_transform, gpu=0)
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+
+    val_data = MyDataLoader(transform=val_transform, trainval='test', data_path='../dataset/',
+                            random_crops=10)
+    val_loader = torch.utils.data.DataLoader(dataset=val_data,
+                                             batch_size=8,
+                                             shuffle=False,
+                                             num_workers=4)
+
+    #test(val_transform, gpu=0)
+    gethreshold(val_loader, val_transform,gpu=None)
